af26fc9bc1265304a0874c1806c925db04bec9c5
Paloma Riquelme <paloma.riquelme.cordero@ciisa.cl>
2022-07-24 13:00:21 -0400

    probando Paloma

--- resources/views/actualizarProducto.blade.php
+++ resources/views/actualizarProducto.blade.php
@@ -1,2 +1,13 @@
-Actualizar producto:
+@extends('layouts.master')
+@section('title','Actualizacion de producto')
+
+@section('header')
+<h2>Actualizar producto:</h2>
+@stop
+
+<div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label">Nombre producto</label>
+      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    </div>
+
 <hr>
